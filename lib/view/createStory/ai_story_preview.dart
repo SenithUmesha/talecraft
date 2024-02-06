a657277b7877ba3ceafd186c07e23ca5bcb924ae
BlackEyedGhouL <34senith@gmail.com>
2024-02-07 01:43:40 +0530

    Add fab and help icon

--- lib/view/createStory/ai_story_preview.dart
+++ lib/view/createStory/ai_story_preview.dart
@@ -20,6 +20,23 @@ class AiStoryPreview extends StatelessWidget {
             child: Scaffold(
           appBar: CustomAppBar(
             title: AppStrings.aiGenaratedStoryPreview,
+            actions: [
+              IconButton(
+                icon: const Icon(
+                  Icons.help_outline_rounded,
+                  color: Colors.black,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: AppColors.black,
+            child: Icon(
+              Icons.arrow_forward_rounded,
+              color: AppColors.white,
+            ),
+            onPressed: () {},
           ),
           body: GestureDetector(
             onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
@@ -34,28 +51,33 @@ class AiStoryPreview extends StatelessWidget {
                         centerLayout: true,
                         enableDelete: false,
                         builder: (context, block) {
-                          return Container(
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: AppColors.white,
-                                  border: Border.all(
-                                      color: (block.data as Block).type ==
-                                              BlockType.story
-                                          ? AppColors.black
-                                          : AppColors.red,
-                                      width: 2)),
-                              padding: const EdgeInsets.all(12),
-                              child: AppWidgets.regularText(
-                                  text: (block.data as Block).type ==
-                                          BlockType.choice
-                                      ? (block.data as Block).text
-                                      : (block.data as Block).shortDescription,
-                                  size: 14.0,
-                                  alignment: TextAlign.center,
-                                  color: AppColors.black,
-                                  weight: FontWeight.w400,
-                                  textOverFlow: TextOverflow.ellipsis,
-                                  maxLines: 1));
+                          return GestureDetector(
+                            onDoubleTap: () =>
+                                controller.showBlockDialog(block.data!),
+                            child: Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: AppColors.white,
+                                    border: Border.all(
+                                        color: (block.data as Block).type ==
+                                                BlockType.story
+                                            ? AppColors.black
+                                            : AppColors.red,
+                                        width: 2)),
+                                padding: const EdgeInsets.all(12),
+                                child: AppWidgets.regularText(
+                                    text: (block.data as Block).type ==
+                                            BlockType.choice
+                                        ? (block.data as Block).text
+                                        : (block.data as Block)
+                                            .shortDescription,
+                                    size: 14.0,
+                                    alignment: TextAlign.center,
+                                    color: AppColors.black,
+                                    weight: FontWeight.w400,
+                                    textOverFlow: TextOverflow.ellipsis,
+                                    maxLines: 1)),
+                          );
                         },
                         onEdgeColor: (n1, n2) {
                           if ((n1.data as Block).type == BlockType.story) {
